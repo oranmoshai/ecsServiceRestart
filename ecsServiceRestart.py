c0e8c483598b58f8ed1f03a7ec7db2cd5556660e
Oran Moshai <oran.moshai@aquasec.com>
2018-09-02 11:53:30 +0300

    Refactor support new ECS forceNewDeployment.

--- ecsServiceRestart.py
+++ ecsServiceRestart.py
@@ -2,129 +2,53 @@ import boto3
 import json
 import fire
 import sys
+import os
 
 #python ecsServiceRestart.py restart --services="app app2" --cluster=test
 class ecsServiceRestart(object):
 
     def __init__(self):
+        try:
+            self.client = boto3.client('ecs',region_name=os.environ['AWS_DEFAULT_REGION'])
+        except Exception as e:
+            print e
+            sys.exit(1)
 
-        self.client = boto3.client('ecs',region_name="us-east-1")
-
-
-    def returnServiceTaskDefinition(self, service, cluster):
+    def updateService(self, service, cluster):
         """
-        Return service attached task definition
+        Restart service
         @INPUT: service cluster
-        @OUTPUT: task-definition or False
+        @OUTPUT boolean
         """
         try:
-            response = self.client.describe_services(cluster=cluster,services=[service])
-            return response['services'][0]['deployments'][0]['taskDefinition']
-        except:
+            response = self.client.update_service(
+            cluster=cluster,
+            service=service,
+            forceNewDeployment=True
+            )
+        except Exception as e:
+            print e
             return False
-
-    def registerTaskDefinition(self, taskDefinitionDescription):
-        """
-        Registering cloned task definition
-        @INPUT: taskDefinitionDescription
-        @OUTPUT: cloned task definition
-        """
-        fargate = False
-        try:
-            taskRoleArn = taskDefinitionDescription['taskDefinition']['taskRoleArn']
-        except:
-            taskRoleArn = ""
-        try:
-            executionRoleArn = taskDefinitionDescription['taskDefinition']['executionRoleArn']
-        except:
-            executionRoleArn = ""
-        try:
-            volumes = taskDefinitionDescription['taskDefinition']['volumes']
-        except:
-            volumes = []
-        try:
-            placementConstraints = taskDefinitionDescription['taskDefinition']['placementConstraints']
-        except:
-            placementConstraints = []
-        try:
-            requiresCompatibilities = taskDefinitionDescription['taskDefinition']['requiresCompatibilities']
-        except:
-            requiresCompatibilities = []
-        try:
-            cpu = taskDefinitionDescription['taskDefinition']['cpu']
-            fargate = True
-        except:
-            cpu = ""
-        try:
-            memory = taskDefinitionDescription['taskDefinition']['memory']
-            fargate = True
-        except:
-            memory = ""
-
-        if fargate == False:
-            response = self.client.register_task_definition(
-            family=taskDefinitionDescription['taskDefinition']['family'],
-            networkMode=taskDefinitionDescription['taskDefinition']['networkMode'],
-            taskRoleArn=taskRoleArn,
-            executionRoleArn=executionRoleArn,
-            containerDefinitions=taskDefinitionDescription['taskDefinition']['containerDefinitions'],
-            volumes=volumes,
-            placementConstraints=placementConstraints,
-            requiresCompatibilities=requiresCompatibilities)
-            return response
-
-        if fargate == True:
-            response = self.client.register_task_definition(
-            family=taskDefinitionDescription['taskDefinition']['family'],
-            networkMode=taskDefinitionDescription['taskDefinition']['networkMode'],
-            taskRoleArn=taskRoleArn,
-            executionRoleArn=executionRoleArn,
-            containerDefinitions=taskDefinitionDescription['taskDefinition']['containerDefinitions'],
-            volumes=volumes,
-            placementConstraints=placementConstraints,
-            requiresCompatibilities=requiresCompatibilities,
-            cpu=cpu,
-            memory=memory)
-            return response
-
-    def updateService(self, service, cluster, taskDefinitionArn):
-        """
-        Update service new task definition
-        @INPUT: service cluster taskDefinitionArn
-        @OUTPUT service desiredCount
-        """
-        response = self.client.update_service(
-        cluster=cluster,
-        service=service,
-        taskDefinition=taskDefinitionArn
-        )
-        return response['service']['desiredCount']
+        return True
 
     def restart(self, services,cluster):
         services = services.split(' ')
 
         for service in services:
             print "Starting restart " + service + " " + cluster
-            #current taskDefinition
-            taskDefinition = self.returnServiceTaskDefinition(service, cluster)
-            if taskDefinition == False:
-                print "Service " + service + " does not exist."
+            #Force new deployment
+            if(self.updateService(service, cluster) == False):
                 sys.exit(1)
-
-            #Describe taskDefinition
-            taskDefinitionDescription = self.client.describe_task_definition(taskDefinition=taskDefinition)
-            #Clone last taskDefinition
-            response = self.registerTaskDefinition(taskDefinitionDescription)
-
-            #Update service with new cloned task difinition
-            self.updateService(service, cluster,response['taskDefinition']['taskDefinitionArn'])
-
             #Wait for service be stable
-            waiter = self.client.get_waiter('services_stable')
-            waiter.wait(
-              cluster=cluster,
-              services=[service]
-            )
+            try:
+                waiter = self.client.get_waiter('services_stable')
+                waiter.wait(
+                  cluster=cluster,
+                  services=[service]
+                )
+            except:
+                print "Failed restart service after 40 checks."
+                sys.exit(1)
             print "Finished restart " + service + " " + cluster
 
 if __name__ == '__main__':
